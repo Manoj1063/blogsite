0bd36a4d8a29fbd21b7a4d06800b0defc17b006c
manoj <pmanojkumar@ami.com>
2021-07-24 20:31:24 +0530

    Intial changes

---
+++ flaskblog/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SECRET_KEY'] = '2a51479d28aea4698484b9d508a98f7b'
+
+# Database connection
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category= 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_POST'] = 100
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+from flaskblog import routes
